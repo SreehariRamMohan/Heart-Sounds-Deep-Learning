a90a0a5063f6de599d6298078f374ede6ac53b64
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2017-11-25 01:01:53 -0800

    Update BPM code

--- AnalyzingHeartbeatForBPM.py
+++ AnalyzingHeartbeatForBPM.py
@@ -14,7 +14,7 @@ def rolmean(dataset, hrw, fs):
     mov_avg = pd.rolling_mean(dataset.hart, window=(int(hrw * fs)))
     avg_hr = (np.mean(dataset.hart))
     mov_avg = [avg_hr if math.isnan(x) else x for x in mov_avg]
-    mov_avg = [x * 1.2 for x in mov_avg]
+    #mov_avg = [x * 1.2 for x in mov_avg]
     dataset['hart_rollingmean'] = mov_avg
 
 
@@ -24,7 +24,7 @@ def detect_peaks(dataset):
     listpos = 0
     for datapoint in dataset.hart:
         rollingmean = dataset.hart_rollingmean[listpos]
-        if (datapoint < rollingmean) and (len(window) < 1):
+        if (datapoint <= rollingmean) and (len(window) <= 1): #Here is the update in (datapoint <= rollingmean)
             listpos += 1
         elif (datapoint > rollingmean):
             window.append(datapoint)
@@ -40,20 +40,52 @@ def detect_peaks(dataset):
 
 
 def calc_RR(dataset, fs):
-    RR_list = []
     peaklist = measures['peaklist']
+    RR_list = []
     cnt = 0
     while (cnt < (len(peaklist) - 1)):
         RR_interval = (peaklist[cnt + 1] - peaklist[cnt])
         ms_dist = ((RR_interval / fs) * 1000.0)
         RR_list.append(ms_dist)
         cnt += 1
+
+    RR_diff = []
+    RR_sqdiff = []
+    cnt = 0
+    while (cnt < (len(RR_list) - 1)):
+        RR_diff.append(abs(RR_list[cnt] - RR_list[cnt + 1]))
+        RR_sqdiff.append(math.pow(RR_list[cnt] - RR_list[cnt + 1], 2))
+        cnt += 1
     measures['RR_list'] = RR_list
+    measures['RR_diff'] = RR_diff
+    measures['RR_sqdiff'] = RR_sqdiff
 
 
-def calc_bpm():
+def calc_ts_measures():
     RR_list = measures['RR_list']
+    RR_diff = measures['RR_diff']
+    RR_sqdiff = measures['RR_sqdiff']
     measures['bpm'] = 60000 / np.mean(RR_list)
+    measures['ibi'] = np.mean(RR_list)
+    measures['sdnn'] = np.std(RR_list)
+    measures['sdsd'] = np.std(RR_diff)
+    measures['rmssd'] = np.sqrt(np.mean(RR_sqdiff))
+    NN20 = [x for x in RR_diff if (x > 20)]
+    NN50 = [x for x in RR_diff if (x > 50)]
+    measures['nn20'] = NN20
+    measures['nn50'] = NN50
+    measures['pnn20'] = float(len(NN20)) / float(len(RR_diff))
+    measures['pnn50'] = float(len(NN50)) / float(len(RR_diff))
+
+
+#Don't forget to update our process() wrapper to include the new function
+def process(dataset, hrw, fs):
+    rolmean(dataset, hrw, fs)
+    detect_peaks(dataset)
+    calc_RR(dataset, fs)
+    calc_ts_measures()
+    plotter(dataset, "My Heartbeat Plot")
+
 
 
 def plotter(dataset, title):
@@ -66,15 +98,6 @@ def plotter(dataset, title):
     plt.legend(loc=4, framealpha=0.6)
     plt.show()
 
-
-def process(dataset, hrw,
-            fs):  # Remember; hrw was the one-sided window size (we used 0.75) and fs was the sample rate (file is recorded at 100Hz)
-    rolmean(dataset, hrw, fs)
-    detect_peaks(dataset)
-    calc_RR(dataset, fs)
-    calc_bpm()
-    plotter(dataset, "My Heartbeat Plot")
-
 import AnalyzingHeartbeatForBPM as hb #Assuming we named the file 'heartbeat.py'
 dataset = hb.get_data("/Users/sreeharirammohan/Desktop/TensorFlow_Programs/Heart-Sounds-Deep-Learning/foo.csv")
 hb.process(dataset, 0.75, 100)
@@ -82,5 +105,13 @@ hb.process(dataset, 0.75, 100)
 #This object contains the dictionary 'measures' with all values in it
 #Now we can also retrieve the BPM value (and later other values) like this:
 bpm = hb.measures['bpm']
+#The module dict now contains all the variables computed over our signal:
+hb.measures['bpm']
+hb.measures['ibi']
+hb.measures['sdnn']
+#etcetera
+
+#Remember that you can get a list of all dictionary entries with "keys()":
+
 #To view all objects in the dictionary, use "keys()" like so:
 print(hb.measures.keys())
