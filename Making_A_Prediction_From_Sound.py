678c4a4eda17170a7ff37443c57ba0492ee38b52
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2017-11-30 17:43:33 -0800

    Added the adding prediction from sound python file

---
+++ Making_A_Prediction_From_Sound.py
@@ -0,0 +1,190 @@
+import pyaudio
+import wave
+import numpy
+import matplotlib.pyplot as plt
+import pylab
+from scipy.io import wavfile
+from scipy.fftpack import fft
+from pydub import AudioSegment
+from PIL import Image
+import io
+import wave, os, glob
+import soundfile as sf
+import os
+from os.path import basename
+import soundfile
+import numpy as np
+np.random.seed(123)  # for reproducibility
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+import os
+import numpy
+import re
+from skimage import io
+import glob
+import numpy as np
+import _pickle as cPickle
+import pickle
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.optimizers import SGD
+from keras import backend as K
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+import keras
+import h5py
+import os
+import numpy
+import re
+from skimage import io
+import glob
+import numpy as np
+import _pickle as cPickle
+import pickle
+from PIL import Image
+from sklearn.model_selection import train_test_split
+import os
+import numpy
+import re
+from skimage import io
+import glob
+import numpy as np
+import _pickle as cPickle
+import pickle
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import os
+import numpy
+import re
+from skimage import io
+import glob
+import numpy as np
+import _pickle as cPickle
+import pickle
+from PIL import Image
+from sklearn.model_selection import train_test_split
+#below dependencies required for mel-spectrogram
+import os
+import matplotlib
+matplotlib.use('Agg') # No pictures displayed 
+import pylab
+import librosa
+import librosa.display
+import numpy as np
+
+#below dependencies required for mel-spectrogram
+import os
+import matplotlib
+matplotlib.use('Agg') # No pictures displayed 
+import pylab
+import librosa
+import librosa.display
+import numpy as np
+
+
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 15
+WAVE_OUTPUT_FILENAME = "/Users/sreeharirammohan/Desktop/output_test.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+def individualWavToMelSpectrogram(myAudioPath, fileNameToSaveTo):
+    sig, fs = librosa.load(myAudioPath)   
+    # make pictures name 
+    save_path = '/Users/sreeharirammohan/Desktop/' + fileNameToSaveTo + '.jpg'
+
+    pylab.axis('off') # no axis
+    pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
+    S = librosa.feature.melspectrogram(y=sig, sr=fs)
+    librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
+    pylab.savefig(save_path, bbox_inches=None, pad_inches=0)
+    pylab.close()
+
+individualWavToMelSpectrogram("/Users/sreeharirammohan/Desktop/output_test.wav", "mic-test")
+
+print("Created mel-spectrogram")
+
+
+def create_model(weights_path=None):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=(3, 640, 480)))
+    print("fail")
+    model.add(Conv2D(64, (3, 3), activation='relu', dim_ordering="th"))
+    print("fail2")
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(2, activation='softmax'))
+    if weights_path:
+        model.load_weights(weights_path)
+    return model
+
+def convert_spectrogram_to_numpy(path_to_spectrogram):
+    img = io.imread(path_to_spectrogram)
+    return img
+
+
+model = create_model('/Users/sreeharirammohan/Desktop/check_point_models/weights-best-031-0.88735.hdf5')
+print("Created model")
+
+
+numpy_image_from_spectrogram = np.array(convert_spectrogram_to_numpy('/Users/sreeharirammohan/Desktop/mic-test.jpg'))
+print("Converted heartbeat data to numpy arr")
+
+numpy_image_from_spectrogram = np.swapaxes(numpy_image_from_spectrogram, 2, 0)
+numpy_image_from_spectrogram = numpy_image_from_spectrogram[None, ...]
+prediction = model.predict_classes(numpy_image_from_spectrogram)
+if(prediction[0] == 0):
+    print("Heartbeat NORMAL")
+else:
+    print("Heartbeat ABNORMAL")
+       
+
+
+
+
